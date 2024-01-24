438858e59265b95c326b5402a4e573f41ab51ea8
Suri1336 <146222294+Suri1336@users.noreply.github.com>
2024-01-24 01:50:24 +0000

    function done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//delete toDo
+//button
+
 
 //create your first component
 const Home = () => {
+	const [toDo,setToDo] = useState([])
+	const [userInput,setUserInput] = useState("")
+
+//type in toDo
+	function addToDo(task){
+let temporaryToDo ={lable:task,done:false}
+setToDo([...toDo,temporaryToDo])
+setUserInput("")
+	}
+
+//delete todo
+function deleteToDo(remove){
+	console.log(remove)
+	let newToDo =toDo.filter((task,index)=>index != remove)
+	setToDo(newToDo)
+}
+//handle enter
+function handleEnter (e,task){
+	if(e.key==="Enter"){
+		addToDo(task)
+	}
+}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="top">
+			<h1>To Do</h1>	
+			<input type="text" value={userInput} onChange={(e)=>setUserInput(e.target.value)} onKeyDown={(e)=>handleEnter(e,userInput)}/>
+			<button className="btn btn-success" onClick={()=> addToDo(userInput)}>Add</button>
+			</div>
+			<div>
+				<ul>
+					{toDo?.map((task,index)=>(
+						<li key={index}>{task.lable} 
+						<button className="btn btn-danger" onClick={()=>deleteToDo(index)}>Delete</button>
+						</li>
+						
+					))}
+				</ul>
+				<p>{toDo.length}Things Left To Do</p>
+			</div>
 		</div>
 	);
 };
